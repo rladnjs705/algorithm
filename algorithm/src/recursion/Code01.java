c62578d8be894dfcf004e95d236588f771466483
jw <jw@jw-PC>
2018-08-01 13:51:08 +0900

    기본셋팅,Recursion

---
+++ algorithm/src/recursion/Code01.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class Code01 {
+	public static void main(String [] args) {
+		func();
+	}
+	
+	public static void func() {
+		System.out.println("Hello");
+		func();
+	}
+}
